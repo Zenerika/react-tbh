dcf2f69a4a535ae4b2b3b1b2d8fa6d9f0d25865e
Aguirre <zenerika@ymail.com>
2019-04-07 10:53:40 -0500

    updated the dashboard css

--- src/components/recycle.js
+++ src/components/recycle.js
@@ -13,3 +13,44 @@
   </div>
 </div>
 </div>
+
+
+// /ignore
+
+<div>
+<div className="tbh-cards">
+  <div className="tbh-row">
+    <div className="tbh-card-item"><h1>To-Do List</h1>
+      <Link to="/todo"><img src={Todo} width="80px" alt="chores"/></Link>
+    </div>
+    <div className="tbh-card-item"><h1>Chores</h1>
+      <Link to="/chores"><img src={Chores} width="80px" alt="bills"/></Link>
+    </div>
+    <div className="tbh-card-item"><h1>Groceries</h1>
+      <Link to="/groceries"><img src={Groceries} width="80px" alt="groceries"/></Link>
+    </div>
+  </div>
+  <div className="columns">
+    <div className="column"><h1>Auto</h1>
+      <Link to="/auto"><img src={Auto} width="80px" alt="chores"/></Link>
+    </div>
+    <div className="column"><h1>Bills</h1>
+      <Link to="/bills"><img src={Bills} width="80px" alt="bills"/></Link>
+    </div>
+    <div className="column"><h1>Pets</h1>
+      <Link to="/pets"><img src={Pets} width="80px" alt="bills"/></Link>
+    </div>
+  </div>
+  <div className="tbh-row">
+    <div className="tbh-card-item"><h1>Medical</h1>
+      <Link to="/medical"><img src={Medical} width="80px" alt="medical"/></Link>
+    </div>
+    <div className="tbh-card-item"><h1>Calendar</h1>
+      <Link to="/calendar"><img src={Calendar} width="80px" alt="calendar"/></Link>
+    </div>
+    <div className="tbh-card-item"><h1>Fitness</h1>
+      <Link to="/fitness"><img src={Fitness} width="80px" alt="fitness"/></Link>
+    </div>
+  </div>
+  </div>
+</div>
