8a14c0ae94b8d2728682d15d3d7052c13c93c1a7
Aguirre <zenerika@ymail.com>
2019-04-07 16:27:18 -0500

    debugging mobile view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -22,32 +22,32 @@ class Dashboard extends Component {
           <div className="tbh-card-item">To-Do List<br></br>
             <Link to="/todo"><img src={Todo} width="80px" alt="to-do list"/></Link>
           </div>
-          <div className="tbh-card-item">Chores
+          <div className="tbh-card-item">Chores<br></br>
             <Link to="/chores"><img src={Chores} width="80px" alt="chores"/></Link>
           </div>
-          <div className="tbh-card-item">Groceries
+          <div className="tbh-card-item">Groceries<br></br>
             <Link to="/groceries"><img src={Groceries} width="80px" alt="groceries"/></Link>
           </div>
         </div>
         <div className="tbh-row">
-          <div className="tbh-card-item"><h1>Auto</h1>
+          <div className="tbh-card-item">Auto<br></br>
             <Link to="/auto"><img src={Auto} width="80px" alt="auto"/></Link>
           </div>
-          <div className="tbh-card-item"><h1>Bills</h1>
+          <div className="tbh-card-item">Bills<br></br>
             <Link to="/bills"><img src={Bills} width="80px" alt="bills"/></Link>
           </div>
-          <div className="tbh-card-item"><h1>Pets</h1>
+          <div className="tbh-card-item">Pets<br></br>
             <Link to="/pets"><img src={Pets} width="80px" alt="pets"/></Link>
           </div>
         </div>
         <div className="tbh-row">
-          <div className="tbh-card-item"><h1>Medical</h1>
+          <div className="tbh-card-item">Medical<br></br>
             <Link to="/medical"><img src={Medical} width="80px" alt="medical"/></Link>
           </div>
-          <div className="tbh-card-item"><h1>Calendar</h1>
+          <div className="tbh-card-item">Calendar<br></br>
             <Link to="/calendar"><img src={Calendar} width="80px" alt="calendar"/></Link>
           </div>
-          <div className="tbh-card-item"><h1>Fitness</h1>
+          <div className="tbh-card-item">Fitness<br></br>
             <Link to="/fitness"><img src={Fitness} width="80px" alt="fitness"/></Link>
           </div>
         </div>
